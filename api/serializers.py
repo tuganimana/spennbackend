ffc398441a7454ab4d2181f753fa9aa7017674f2
Telesphore <tuganimana01@gmail.com>
2020-09-23 21:10:40 +0200

    first commit

--- api/serializers.py
+++ api/serializers.py
@@ -30,7 +30,7 @@ class UserSerializerWithToken(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('token', 'username', 'password', 'first_name')
+        fields = ('token', 'username', 'password', 'first_name','last_name')
 
 
 
