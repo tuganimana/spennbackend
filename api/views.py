be83bbc3bddcac7d14887e7f958b5a54e964dce9
Telesphore <tuganimana01@gmail.com>
2020-09-23 18:27:55 +0200

    first commit

--- api/views.py
+++ api/views.py
@@ -9,8 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status,viewsets
 from rest_framework.response import Response
 import os, hashlib, warnings, requests, json
-import base64
-from Crypto.Cipher import DES3
+
 
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated,AllowAny
