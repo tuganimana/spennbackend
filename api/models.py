cca548d7b0714fa1bf4ad95bd6625ec277b13752
Telesphore <tuganimana01@gmail.com>
2020-09-24 02:32:50 +0200

    first commit

--- api/models.py
+++ api/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Moneysend(models.Model):
-    sender =models.CharField(max_length=255),
-    receiver =models.CharField(max_length=254),
-    amount = models.FloatField(),
+    sender =models.CharField(max_length=255)
+    receiver =models.CharField(max_length=254)
+    amount = models.CharField(max_length=255, default=0)
     description = models.CharField(max_length=255,default="Payment Processed on")
     paymenton =models.DateTimeField(auto_now=True)
+
 
 class Profile(models.Model):
    
@@ -17,3 +18,10 @@ class Profile(models.Model):
    
     def __str__(self):
         return self.telephone
+
+class Paybills(models.Model):
+    sender =models.CharField(max_length=255)
+    receiver =models.CharField(max_length=254)
+    amount = models.CharField(max_length=255, default=0)
+    description = models.CharField(max_length=255,default="Payment Processed on")
+    paymenton =models.DateTimeField(auto_now=True)
